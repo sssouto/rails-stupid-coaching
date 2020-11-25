919ebde718b78831d987fe029a3a296ab7fe362d
sander souto <sander.soares@yahoo.com.br>
2020-11-25 15:28:12 -0300

    Stupid Coach Challenge

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    user_input = params[:question]
+    @answer = if user_input == 'I am going to work'
+                'Great!'
+              elsif user_input.include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
